8f4382cdfd0dd6082dda5cf16532286f1b0e5ab2
txdvil <t.o.m.m.x.3.1@gmail.com>
2024-03-20 10:03:27 +0900

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -21,7 +21,7 @@ logger.setLevel(logging.DEBUG)
 # ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://tiktokrss.conoroneill.com/"
+ghPagesURL = "https://txdvil.github.io/tiktok-rss"
 
 maxItems = 5
 
